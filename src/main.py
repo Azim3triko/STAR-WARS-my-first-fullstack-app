6cb21be42d3040845471df7945b96a5dd4ae88e4
Azim3triko <azim3triko@protonmail.com>
2022-07-05 00:48:03 +0000

    faltan los endpoints

--- src/main.py
+++ src/main.py
@@ -33,11 +33,10 @@ def sitemap():
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users = user.query.all()
+    all_users = list(map(lambda x: x.serialize(user), people_query))
 
-    return jsonify(response_body), 200
+    return jsonify(users), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    character_favorite = db.relationship('CharacterFavorite', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +16,74 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "CharacterFavorite": list(map(lambda x: x.serialize(), self.character_favorite))
+        }
+            # do not serialize the password, its a security breach
+        
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    birth_year = db.Column(db.String(80), nullable=False)
+  
+    def __repr__(self):
+        return '<Character %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            # do not serialize the password, its a security breach
+        }
+
+class CharacterFavorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship('User', lazy=True)
+    character_id = db.Column(db.Integer, db.ForeignKey("character.id"))
+    character = db.relationship('Character', lazy=True )
+       
+    def __repr__(self):
+        return '<Character %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character.id": self.character_id,
+         
+            # do not serialize the password, its a security breach
+        }
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    climate = db.Column(db.String(80), nullable=False)
+  
+    def __repr__(self):
+        return '<Planet %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            # do not serialize the password, its a security breach
+        }
+
+class PlanetFavorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship('User', lazy=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"))
+    planet = db.relationship('Planet', lazy=True )
+       
+    def __repr__(self):
+        return '<Planet %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet.id": self.character_id,
+         
             # do not serialize the password, its a security breach
         }
